9a756e6d0b47d4cfc4e6b441899574a5f095ccef
TheCengo <cengoyilmaz801@gmail.com>
2021-02-14 20:38:35 +0300

    url changed

--- src/components/services/SinavService.js
+++ src/components/services/SinavService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const SESSION_URL = "https://biga-aci-be.herokuapp.com/sinav/activeType";
-const SESSION_URL = "http://localhost:8086/sinav/activeType";
+const SESSION_URL = "https://biga-aci-be.herokuapp.com/sinav/activeType";
+//const SESSION_URL = "http://localhost:8086/sinav/activeType";
 class SinavService {
 
     getSinavTypes() {
